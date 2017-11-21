ab844256d0ca2ef930de928be6122ec26b1c3113
mumu-two <942881088@qq.com>
2017-11-21 22:18:36 +0800

    第一次

---
+++ 登录/12.js
@@ -0,0 +1,17 @@
+// JavaScript Document
+function onclick()
+{
+var Name=document.forms[0].name.value;
+var sex=document.forms[0].sex.value;
+var password=document.forms[0].password.value;
+var email=document.forms[0].email.value;
+var age=document.forms[0].age.value;
+if(Name==null||sex==null){
+if(Name==null){
+	alert("请输入会员名")；
+}
+alert("请选择性别")；
+}
+if(password.){
+	
+}
